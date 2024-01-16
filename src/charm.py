f319a9190382be75f47c340b6b878d3c42b0b858
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-01-16 13:48:05 -0500

    feat: Installs vault snap

--- src/charm.py
+++ src/charm.py
@@ -7,11 +7,16 @@
 
 import logging
 
+from charms.operator_libs_linux.v1 import snap
 from ops.charm import CharmBase, InstallEvent
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, MaintenanceStatus
 
 logger = logging.getLogger(__name__)
+
+VAULT_SNAP_NAME = "vault"
+VAULT_SNAP_CHANNEL = "1.10/stable"
+VAULT_SNAP_REVISION = 2091
 
 
 class VaultOperatorCharm(CharmBase):
@@ -19,11 +24,28 @@ class VaultOperatorCharm(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.install, self._on_install)
+        self.framework.observe(self.on.install, self._configure)
+        self.framework.observe(self.on.update_status, self._configure)
 
-    def _on_install(self, event: InstallEvent):
-        """Handle installation."""
+    def _configure(self, event: InstallEvent):
+        """Handle Vault installation."""
+        self.unit.status = MaintenanceStatus("Installing Vault")
+        self._install_vault_snap()
         self.unit.status = ActiveStatus()
+
+    def _install_vault_snap(self) -> None:
+        """Installs the Vault snap in the machine."""
+        try:
+            snap_cache = snap.SnapCache()
+            vault_snap = snap_cache[VAULT_SNAP_NAME]
+            vault_snap.ensure(
+                snap.SnapState.Latest, channel=VAULT_SNAP_CHANNEL, revision=VAULT_SNAP_REVISION
+            )
+            vault_snap.hold()
+
+        except snap.SnapError as e:
+            logger.error("An exception occurred when installing Vault. Reason: %s", str(e))
+            raise
 
 
 if __name__ == "__main__":  # pragma: nocover
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -2,10 +2,27 @@
 # See LICENSE file for licensing details.
 
 import unittest
+from unittest.mock import patch
 
 import ops
 import ops.testing
 from charm import VaultOperatorCharm
+from charms.operator_libs_linux.v1.snap import SnapState
+
+
+class MockSnapObject:
+    def __init__(self, name):
+        self.name = name
+        self.ensure_called = False
+        self.ensure_called_with = None
+        self.hold_called = False
+
+    def ensure(self, state, channel, revision):
+        self.ensure_called = True
+        self.ensure_called_with = (state, channel, revision)
+
+    def hold(self):
+        self.hold_called = True
 
 
 class TestCharm(unittest.TestCase):
@@ -14,5 +31,18 @@ class TestCharm(unittest.TestCase):
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
 
-    def test_given_when_then(self):
-        pass
+    @patch("charms.operator_libs_linux.v1.snap.SnapCache")
+    def test_given_vault_snap_uninstalled_when_configure_then_vault_snap_installed(
+        self, mock_snap_cache
+    ):
+        vault_snap = MockSnapObject("vault")
+        snap_cache = {"vault": vault_snap}
+        mock_snap_cache.return_value = snap_cache
+
+        self.harness.charm.on.install.emit()
+
+        mock_snap_cache.assert_called_once_with()
+
+        assert vault_snap.ensure_called
+        assert vault_snap.ensure_called_with == (SnapState.Latest, "1.10/stable", 2091)
+        assert vault_snap.hold_called
