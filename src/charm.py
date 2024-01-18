557b9e413d7d9f8c82d3ebae781876236e219e5f
Guillaume Belanger <guillaume.belanger@canonical.com>
2024-01-18 11:47:54 -0500

    feat: Creates vault config.hcl file (#11)
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- src/charm.py
+++ src/charm.py
@@ -1,36 +1,163 @@
 #!/usr/bin/env python3
-# Copyright 2023 Canonical Ltd.
+# Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 
 """A machine charm for Vault."""
 
 import logging
+import os
+from typing import Optional
 
+import hcl  # type: ignore[import-untyped]
 from charms.operator_libs_linux.v1 import snap
-from ops.charm import CharmBase, InstallEvent
+from jinja2 import Environment, FileSystemLoader
+from ops.charm import CharmBase
 from ops.main import main
-from ops.model import ActiveStatus, MaintenanceStatus
+from ops.model import ActiveStatus, MaintenanceStatus, ModelError, WaitingStatus
 
 logger = logging.getLogger(__name__)
 
+CONFIG_TEMPLATE_DIR_PATH = "src/templates/"
+CONFIG_TEMPLATE_NAME = "vault.hcl.j2"
+PEER_RELATION_NAME = "vault-peers"
+VAULT_CONFIG_PATH = "/var/snap/vault/common/config"
+VAULT_CONFIG_FILE_NAME = "vault.hcl"
+VAULT_PORT = 8200
+VAULT_CLUSTER_PORT = 8201
 VAULT_SNAP_NAME = "vault"
 VAULT_SNAP_CHANNEL = "1.12/stable"
 VAULT_SNAP_REVISION = 2166
+VAULT_STORAGE_PATH = "/var/snap/vault/common/raft"
+
+
+def render_vault_config_file(
+    default_lease_ttl: str,
+    max_lease_ttl: str,
+    cluster_address: str,
+    api_address: str,
+    tcp_address: str,
+    raft_storage_path: str,
+    node_id: str,
+) -> str:
+    """Render the Vault config file."""
+    jinja2_environment = Environment(loader=FileSystemLoader(CONFIG_TEMPLATE_DIR_PATH))
+    template = jinja2_environment.get_template(CONFIG_TEMPLATE_NAME)
+    content = template.render(
+        default_lease_ttl=default_lease_ttl,
+        max_lease_ttl=max_lease_ttl,
+        cluster_address=cluster_address,
+        api_address=api_address,
+        tcp_address=tcp_address,
+        raft_storage_path=raft_storage_path,
+        node_id=node_id,
+    )
+    return content
+
+
+class Machine:
+    """A class to interact with a unit machine.
+
+    This class has the same method signatures as Pebble API in the Ops
+    Library. This is to improve consistency between the Machine and Kubernetes
+    versions of the charm.
+    """
+
+    def exists(self, path: str) -> bool:
+        """Check if a file exists.
+
+        Args:
+            path: The path of the file
+
+        Returns:
+            bool: Whether the file exists
+        """
+        return os.path.isfile(path)
+
+    def pull(self, path: str) -> str:
+        """Get the content of a file.
+
+        Args:
+            path: The path of the file
+
+        Returns:
+            str: The content of the file
+        """
+        with open(path, "r") as read_file:
+            return read_file.read()
+
+    def push(self, path: str, source: str) -> None:
+        """Pushes a file to the unit.
+
+        Args:
+            path: The path of the file
+            source: The contents of the file to be pushed
+        """
+        with open(path, "w") as write_file:
+            write_file.write(source)
+            logger.info("Pushed file %s", path)
+
+
+def config_file_content_matches(existing_content: str, new_content: str) -> bool:
+    """Return whether two Vault config file contents match.
+
+    We check if the retry_join addresses match, and then we check if the rest of the config
+    file matches.
+
+    Returns:
+        bool: Whether the vault config file content matches
+    """
+    existing_config_hcl = hcl.loads(existing_content)
+    new_content_hcl = hcl.loads(new_content)
+    if not existing_config_hcl:
+        logger.info("Existing config file is empty")
+        return existing_config_hcl == new_content_hcl
+    if not new_content_hcl:
+        logger.info("New config file is empty")
+        return existing_config_hcl == new_content_hcl
+
+    new_retry_joins = new_content_hcl["storage"]["raft"].pop("retry_join", [])
+    existing_retry_joins = existing_config_hcl["storage"]["raft"].pop("retry_join", [])
+
+    # If there is only one retry join, it is a dict
+    if isinstance(new_retry_joins, dict):
+        new_retry_joins = [new_retry_joins]
+    if isinstance(existing_retry_joins, dict):
+        existing_retry_joins = [existing_retry_joins]
+
+    new_retry_join_api_addresses = {address["leader_api_addr"] for address in new_retry_joins}
+    existing_retry_join_api_addresses = {
+        address["leader_api_addr"] for address in existing_retry_joins
+    }
+    return (
+        new_retry_join_api_addresses == existing_retry_join_api_addresses
+        and new_content_hcl == existing_config_hcl
+    )
 
 
 class VaultOperatorCharm(CharmBase):
-    """Charm the service."""
+    """Machine Charm for Vault."""
 
     def __init__(self, *args):
         super().__init__(*args)
+        self.machine = Machine()
         self.framework.observe(self.on.install, self._configure)
         self.framework.observe(self.on.update_status, self._configure)
+        self.framework.observe(self.on.config_changed, self._configure)
 
-    def _configure(self, event: InstallEvent):
-        """Handle Vault installation."""
+    def _configure(self, _):
+        """Handle Vault installation.
+
+        This includes:
+          - Installing the Vault snap
+          - Generating the Vault config file
+        """
+        if not self._bind_address:
+            self.unit.status = WaitingStatus("Waiting for bind address")
+            return
         self.unit.status = MaintenanceStatus("Installing Vault")
         self._install_vault_snap()
+        self._generate_vault_config_file()
         self.unit.status = ActiveStatus()
 
     def _install_vault_snap(self) -> None:
@@ -46,6 +173,80 @@ class VaultOperatorCharm(CharmBase):
         except snap.SnapError as e:
             logger.error("An exception occurred when installing Vault. Reason: %s", str(e))
             raise
+
+    def _generate_vault_config_file(self) -> None:
+        """Create the Vault config file and push it to the Machine."""
+        if not self._cluster_address:
+            logger.warning("Cluster address not found")
+            return
+        if not self._api_address:
+            logger.warning("API address not found")
+            return
+        content = render_vault_config_file(
+            default_lease_ttl=self.model.config["default_lease_ttl"],
+            max_lease_ttl=self.model.config["max_lease_ttl"],
+            cluster_address=self._cluster_address,
+            api_address=self._api_address,
+            tcp_address=f"[::]:{VAULT_PORT}",
+            raft_storage_path=VAULT_STORAGE_PATH,
+            node_id=self._node_id,
+        )
+        existing_content = ""
+        vault_config_file_path = f"{VAULT_CONFIG_PATH}/{VAULT_CONFIG_FILE_NAME}"
+        if self.machine.exists(path=vault_config_file_path):
+            existing_content = self.machine.pull(path=vault_config_file_path)
+
+        if not config_file_content_matches(existing_content=existing_content, new_content=content):
+            self.machine.push(
+                path=vault_config_file_path,
+                source=content,
+            )
+
+    @property
+    def _bind_address(self) -> Optional[str]:
+        """Fetches bind address from peer relation and returns it.
+
+        Returns:
+            str: Bind address
+        """
+        peer_relation = self.model.get_relation(PEER_RELATION_NAME)
+        if not peer_relation:
+            return None
+        try:
+            binding = self.model.get_binding(peer_relation)
+            if not binding or not binding.network.bind_address:
+                return None
+            return str(binding.network.bind_address)
+        except ModelError:
+            return None
+
+    @property
+    def _api_address(self) -> Optional[str]:
+        """Returns the IP with the https schema and vault port.
+
+        Example: "https://1.2.3.4:8200"
+        """
+        if not self._bind_address:
+            return None
+        return f"https://{self._bind_address}:{VAULT_PORT}"
+
+    @property
+    def _cluster_address(self) -> Optional[str]:
+        """Return the IP with the https schema and vault port.
+
+        Example: "https://1.2.3.4:8201"
+        """
+        if not self._bind_address:
+            return None
+        return f"https://{self._bind_address}:{VAULT_CLUSTER_PORT}"
+
+    @property
+    def _node_id(self) -> str:
+        """Return node id for vault.
+
+        Example of node id: "vault-0"
+        """
+        return f"{self.model.name}-{self.unit.name}"
 
 
 if __name__ == "__main__":  # pragma: nocover
---
+++ tests/unit/config.hcl
@@ -0,0 +1,20 @@
+ui      = true
+storage "raft" {
+  path= "/var/snap/vault/common/raft"
+  node_id = "whatever-vault/0"
+  }
+listener "tcp" {
+  telemetry {
+    unauthenticated_metrics_access = true
+  }
+  address       = "[::]:8200"
+}
+default_lease_ttl = "168h"
+max_lease_ttl     = "720h"
+disable_mlock     = true
+cluster_addr      = "https://1.2.1.2:8201"
+api_addr          = "https://1.2.1.2:8200"
+telemetry {
+  disable_hostname = true
+  prometheus_retention_time = "12h"
+}
