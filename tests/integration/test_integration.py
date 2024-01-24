8ceff2320503c9893cf5469e4f734ae1bb6ed8ad
yazansalti <yazan.salti@canonical.com>
2024-01-24 16:36:45 +0400

    Adds a simple integration test

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -14,6 +14,7 @@ logger = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
+GRAFANA_AGENT_APPLICATION_NAME = "grafana-agent"
 
 
 @pytest.fixture(scope="module")
@@ -28,12 +29,42 @@ async def build_and_deploy(ops_test: OpsTest):
         trust=True,
     )
 
+    pytest.mark.abort_on_fail
+    @pytest.fixture(scope="module")
+    async def deploy_grafana_agent(ops_test: OpsTest) -> None:
+        """Deploys grafana-agent-operator.
+
+        Args:
+            ops_test: Ops test Framework.
+        """
+        assert ops_test.model
+        await ops_test.model.deploy(
+            GRAFANA_AGENT_APPLICATION_NAME,
+            application_name=GRAFANA_AGENT_APPLICATION_NAME,
+            trust=True,
+        )
 
 @pytest.mark.abort_on_fail
 async def test_given_charm_build_when_deploy_then_status_active(
     ops_test: OpsTest, build_and_deploy
 ):
     assert ops_test.model
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME],
+        status="active",
+        timeout=1000,
+    )
+
+
+@pytest.mark.abort_on_fail
+async def test_given_grafana_agent_deployed_when_relate_to_grafana_agent_then_status_is_active(
+    ops_test: OpsTest, build_and_deploy, deploy_grafana_agent
+):
+    assert ops_test.model
+    await ops_test.model.integrate(
+        APP_NAME,
+        GRAFANA_AGENT_APPLICATION_NAME,
+    )
     await ops_test.model.wait_for_idle(
         apps=[APP_NAME],
         status="active",
