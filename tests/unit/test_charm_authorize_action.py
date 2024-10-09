4f894482dba3da69662eb25274c35424301b9579
yazansalti <yazan.salti@canonical.com>
2024-10-09 15:59:17 +0400

    Refactor test_charm_authorize_action.py

--- tests/unit/test_charm_authorize_action.py
+++ tests/unit/test_charm_authorize_action.py
@@ -4,6 +4,8 @@
 
 
 import scenario
+from ops.testing import ActionFailed
+import pytest
 from charms.vault_k8s.v0.vault_client import AuditDeviceType
 
 from tests.unit.fixtures import VaultCharmFixtures
@@ -14,31 +16,19 @@ class TestCharmAuthorizeAction(VaultCharmFixtures):
         state_in = scenario.State(
             leader=False,
         )
-        action = scenario.Action(
-            name="authorize-charm",
-        )
 
-        action_output = self.ctx.run_action(action, state_in)
-
-        assert action_output.success is False
-        assert action_output.failure == "This action can only be run by the leader unit"
+        with pytest.raises(ActionFailed) as e:
+            self.ctx.run(self.ctx.on.action("authorize-charm"), state_in)
+        assert e.value.message == "This action can only be run by the leader unit"
 
     def test_given_secret_id_not_found_when_authorize_charm_then_fails(self):
         state_in = scenario.State(
             leader=True,
         )
-        action = scenario.Action(
-            name="authorize-charm",
-            params={"secret-id": "my secret id"},
-        )
 
-        action_output = self.ctx.run_action(action, state_in)
-
-        assert action_output.success is False
-        assert (
-            action_output.failure
-            == "The secret id provided could not be found by the charm. Please grant the token secret to the charm."
-        )
+        with pytest.raises(ActionFailed) as e:
+            self.ctx.run(self.ctx.on.action("authorize-charm", params={"secret-id": "my secret id"}), state_in)
+        assert e.value.message == "The secret id provided could not be found by the charm. Please grant the token secret to the charm."
 
     def test_given_api_address_unavailable_when_authorize_charm_then_fails(self):
         self.mock_vault.configure_mock(
@@ -47,24 +37,23 @@ class TestCharmAuthorizeAction(VaultCharmFixtures):
             },
         )
         approle_secret = scenario.Secret(
-            id="0",
             label="vault-approle-auth-details",
-            contents={0: {"role-id": "role id", "secret-id": "secret id"}},
+            tracked_content={"role-id": "role id", "secret-id": "secret id"},
         )
         state_in = scenario.State(
             leader=True,
             secrets=[approle_secret],
-            networks={"vault-peers": scenario.Network.default(private_address="")},
-        )
-        action = scenario.Action(
-            name="authorize-charm",
-            params={"secret-id": approle_secret.id},
+            networks={
+                scenario.Network(
+                    "vault-peers",
+                    bind_addresses=[scenario.BindAddress([scenario.Address("")])],
+                )
+            },
         )
 
-        action_output = self.ctx.run_action(action, state_in)
-
-        assert action_output.success is False
-        assert action_output.failure == "API address is not available."
+        with pytest.raises(ActionFailed) as e:
+            self.ctx.run(self.ctx.on.action("authorize-charm", params={"secret-id": approle_secret.id}), state_in)
+        assert e.value.message == "API address is not available."
 
     def test_given_ca_certificate_unavailable_when_authorize_charm_then_fails(self):
         self.mock_tls.configure_mock(
@@ -73,9 +62,8 @@ class TestCharmAuthorizeAction(VaultCharmFixtures):
             },
         )
         approle_secret = scenario.Secret(
-            id="0",
             label="vault-approle-auth-details",
-            contents={0: {"role-id": "role id", "secret-id": "secret id"}},
+            tracked_content={"role-id": "role id", "secret-id": "secret id"},
         )
         peer_relation = scenario.PeerRelation(
             endpoint="vault-peers",
@@ -84,20 +72,17 @@ class TestCharmAuthorizeAction(VaultCharmFixtures):
             leader=True,
             secrets=[approle_secret],
             relations=[peer_relation],
-            networks={"vault-peers": scenario.Network.default(private_address="1.2.1.2")},
-        )
-        action = scenario.Action(
-            name="authorize-charm",
-            params={"secret-id": approle_secret.id},
+            networks={
+                scenario.Network(
+                    "vault-peers",
+                    bind_addresses=[scenario.BindAddress([scenario.Address("1.2.1.2")])],
+                )
+            },
         )
 
-        action_output = self.ctx.run_action(action, state_in)
-
-        assert action_output.success is False
-        assert (
-            action_output.failure
-            == "CA certificate is not available in the charm. Something is wrong."
-        )
+        with pytest.raises(ActionFailed) as e:
+            self.ctx.run(self.ctx.on.action("authorize-charm", params={"secret-id": approle_secret.id}), state_in)
+        assert e.value.message == "CA certificate is not available in the charm. Something is wrong."
 
     def test_given_when_authorize_charm_then_charm_is_authorized(self):
         self.mock_vault.configure_mock(
@@ -108,8 +93,7 @@ class TestCharmAuthorizeAction(VaultCharmFixtures):
             },
         )
         user_provided_secret = scenario.Secret(
-            id="0",
-            contents={0: {"token": "my token"}},
+            tracked_content={"token": "my token"},
         )
         peer_relation = scenario.PeerRelation(
             endpoint="vault-peers",
@@ -118,14 +102,14 @@ class TestCharmAuthorizeAction(VaultCharmFixtures):
             leader=True,
             secrets=[user_provided_secret],
             relations=[peer_relation],
-            networks={"vault-peers": scenario.Network.default(private_address="1.2.1.2")},
+            networks={
+                scenario.Network(
+                    "vault-peers",
+                    bind_addresses=[scenario.BindAddress([scenario.Address("1.2.1.2")])],
+                )
+            },
         )
-        action = scenario.Action(
-            name="authorize-charm",
-            params={"secret-id": user_provided_secret.id},
-        )
-
-        action_output = self.ctx.run_action(action, state_in)
+        out_state = self.ctx.run(self.ctx.on.action("authorize-charm", params={"secret-id": user_provided_secret.id}), state_in)
 
         self.mock_vault.enable_audit_device.assert_called_once_with(
             device_type=AuditDeviceType.FILE, path="stdout"
@@ -141,14 +125,10 @@ class TestCharmAuthorizeAction(VaultCharmFixtures):
             token_ttl="1h",
             token_max_ttl="1h",
         )
-        assert action_output.success is True
-        assert action_output.results == {
+        assert self.ctx.action_results == {
             "result": "Charm authorized successfully. You may now remove the secret."
         }
-        assert action_output.state.secrets[1].label == "vault-approle-auth-details"
-        assert action_output.state.secrets[1].contents == {
-            0: {
-                "role-id": "my-role-id",
-                "secret-id": "my-secret-id",
-            }
+        assert out_state.get_secret(label="vault-approle-auth-details").tracked_content == {
+            "role-id": "my-role-id",
+            "secret-id": "my-secret-id",
         }
